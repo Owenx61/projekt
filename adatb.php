c9f3500e23d34c7085a783e0d75999872659f0f5
Owenx61 <99092564+Owenx61@users.noreply.github.com>
2022-11-15 13:25:36 +0100

    Add files via upload

---
+++ adatb.php
@@ -0,0 +1,49 @@
+<?php
+/*foreach ($_POST as $key => $value) {
+    echo "<tr>";
+    echo "<td>";
+    echo $key;
+    echo "</td>";
+    echo "<td>";
+    echo $value;
+    echo "</td>";
+    echo "</tr>";
+}*/
+
+if (isset($_POST["mentes"])){
+    $conn = new mysqli("localhost", "root", "","projekt");
+
+    if($conn->connect_error){
+        echo "Nem sikerült csatlakozni az adatbázis szerverhez.";
+    }
+}
+
+$bong = trim($_POST["browser"]);
+$ip = trim($_POST["ip"]);
+$orszag = trim($_POST["orsz"]); 
+$opr = trim($_POST["os"]);
+
+$keres = "INSERT INTO adatok (adatok.bongeszo, adatok.Ip_cim, adatok.orszag, adatok.oprendsz) VALUES ('". $bong ."','". $ip ."', '". $orszag ."','". $opr ."');";
+    $result = $conn->query($keres);
+?>
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Document</title>
+    </head>
+    <body>
+        <form action = '' method= 'post'>
+            <div class="browser">Böngésző: <input readonly value='' id = "browser" name ="browser"></div>
+            <div class="version">Publikus IP cím: <input readonly value = '' id = "ip" name = "ip"></div>
+            <div class="orszag">Ország: <input readonly value = '' id = "orszag" name = "orsz"></div>
+            <div class="os">Op. rendsz.: <input readonly value = '' id ="os" name = "os"></div>
+            
+        </form>
+        <script src="index.js"></script>
+        
+    </body> 
+    </html>
---
+++ index.js
@@ -0,0 +1,54 @@
+var browserName = (function (agent) {
+    switch (true) {
+        case agent.indexOf("edge") > -1: return "MS Edge";
+        case agent.indexOf("edg/") > -1: return "Edge ( chromium based)";
+        case agent.indexOf("opr") > -1 && !!window.opr: return "Opera";
+        case agent.indexOf("chrome") > -1 && !!window.chrome: return "Chrome";
+        case agent.indexOf("trident") > -1: return "MS IE";
+        case agent.indexOf("firefox") > -1: return "Mozilla Firefox";
+        case agent.indexOf("safari") > -1: return "Safari";
+        default: return "other";
+    }
+})(window.navigator.userAgent.toLowerCase());
+
+document.getElementById("browser").value=browserName;  
+//console.log(window.navigator.userAgent)
+function OP() {
+    var userAgent = window.navigator.userAgent,
+        platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
+        macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+        os = null;
+  
+    if (macosPlatforms.indexOf(platform) !== -1) {
+      os = 'Mac OS';
+    } else if (iosPlatforms.indexOf(platform) !== -1) {
+      os = 'iOS';
+    } else if (windowsPlatforms.indexOf(platform) !== -1) {
+      os = 'Windows';
+    } else if (/Android/.test(userAgent)) {
+      os = 'Android';
+    } else if (/Linux/.test(platform)) {
+      os = 'Linux';
+    }
+  
+    return os;
+  }
+
+  asd = OP()
+  document.getElementById('os').value = asd
+
+
+
+fetch('https://ipinfo.io')
+  .then((res) => res.json())
+  .then((res) => {
+    document.getElementById("ip").value = res.query
+    document.getElementById("orszag").value = res.country
+  })
+  gomb = document.getElementById("ment")
+setTimeout(() => {
+
+  gomb.click()
+}, 5000);
